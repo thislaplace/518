4d182edb44937c1b5f422fb72ee6c06a113edc82
thislaplace <1667346276@qq.com>
2018-06-08 21:11:44 +0800

    数据读取并保存

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 518
+
+用户(uid) 博文(mid) 发博时间(time) 转发数(forward_count) 评论数(comment_count) 赞数(like_count) 内容(content)
+用户(uid)作为一级hash表的key值 value存储一个按时间排好序的list表
+每一个博文(mid)又作为二级hash表的key值，value按顺序存储【转发数(forward_count) 评论数(comment_count) 赞数(like_count)】
+博文内容不做处理
--- weibo_predict.py
+++ weibo_predict.py
@@ -1,23 +1,54 @@
 #coding=utf-8
 import numpy as np
-import pandas as pd
 import sklearn
+import time
+import pickle
 
-# 用户(uid) 博文(mid) 发博时间(time) 转发数(forward_count) 评论数(comment_count) 赞数(like_count) 内容(content)
 
+data_path = 'E:\\code\\python3.x\\天池\\新浪微博\\Weibo Data\\weibo_train_data.txt'
+result   = 'E:\\code\\python3.x\\天池\\新浪微博\\Weibo Data\\result.txt'
+data_hash = dict()
 
-file_handle = open('E:\\code\\python3.x\\天池\\新浪微博\\Weibo Data\\weibo_predict_data.txt', 'rb')
-result_handle = open('E:\\code\\python3.x\\天池\\新浪微博\\Weibo Data\\result.txt', 'w')
-lines = file_handle.readlines()
-data = list()
-for line in lines:
-	line = line.strip().decode()
-	line = line.split('\t')
-	line = line[:2]
-	data.append(line)
-file_handle.close()
+''''' 
+* datestr转换成secs 
+* 将时间字符串转化为秒("2012-07-20 00:00:00"->1342713600.0) 
+* 
+'''  
+def datestr2secs(datestr):
+	time.strptime(datestr, '%Y-%m-%d %H:%M:%S')
+	return time.mktime(time.strptime(datestr,'%Y-%m-%d %H:%M:%S'))
+	
 
-for line in data:
-	result_handle.write(line[0]+'\t'+line[1]+'\t0'+',0,0\n')
-
-result_handle.close()
+def read_data():
+	global data_hash
+	global reasult
+	global data_path
+	
+	data_handle = open(data_path, 'rb')
+	lines = data_handle.readlines()
+	data_handle.close()
+	
+	for line in lines:
+		line = line.strip().decode()
+		line = line.split('\t')
+		
+		"""读取数据"""
+		uid   = line[0]
+		mid   = line[1]
+		time  = str(line[2])
+		forward_count = line[3]
+		comment_count = line[4]
+		like_count    = line[5]
+		content       = line[6]
+		
+		second = datestr2secs(time)
+		
+		if not uid in data_hash:
+			data_hash[uid] = []
+		data_hash[uid].append([second, forward_count, comment_count, like_count])
+	
+	result_handle = open(result, 'wb')
+	pickle.dump(data_hash, result_handle)
+	result_handle.close()
+	
+read_data()
